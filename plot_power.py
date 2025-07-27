0e60352b864a439a590e13206a43003d0806a1e7
Grant Wilkins <t-gwilkins@microsoft.com>
2025-07-26 18:15:56 -0700

    plotting stuff

--- plot_power.py
+++ plot_power.py
@@ -3,11 +3,31 @@
 Plot GPU power consumption from the training trace
 """
 
+import matplotlib
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import pandas as pd
 
-plt.switch_backend("agg")  # Use a non-interactive backend for script execution
+plt.switch_backend("agg")  # Use a non-interactive backend for script executionv
+
+
+matplotlib.rcParams["pdf.fonttype"] = 42
+
+matplotlib.rcParams["ps.fonttype"] = 42
+
+plt.rcParams.update(
+    {
+        "text.usetex": True,  # Use LaTeX to write all text
+        "font.family": "serif",
+        "font.serif": ["Linux Libertine O"],  # Specify the Libertine font
+        "axes.labelsize": 9,  # LaTeX default is 10pt font.
+        "font.size": 12,
+        "legend.fontsize": 12,
+        "xtick.labelsize": 10,
+        "ytick.labelsize": 10,
+        "text.latex.preamble": r"\usepackage{libertine} \usepackage[libertine]{newtxmath}",  # Load libertine font
+    }
+)
 
 
 def parse_power_data(csv_file):
@@ -55,7 +75,7 @@ def plot_power_trace(df, output_file=None):
     plt.figure(figsize=(5, 3))
 
     # Plot total power consumption
-    plt.plot(df["time_seconds"], df["power_watts"])
+    plt.plot(df["time_seconds"], df["power_watts"], color="green")
     plt.ylabel("Server Power (W)", fontsize=12)
     plt.xlabel("Time (s)", fontsize=12)
     plt.xlim(0, df["time_seconds"].max())
@@ -72,8 +92,8 @@ def plot_power_trace(df, output_file=None):
 
 
 def main():
-    csv_file = "power-trace_2025-07-26-23-32-09.csv"
-    output_file = "power_trace_plot.pdf"
+    csv_file = "ckpt1000.csv"
+    output_file = "power_trace_plot_1000epoch.pdf"
 
     print(f"Processing power data from: {csv_file}")
 
@@ -87,7 +107,7 @@ def main():
     # Create the plot
     plot_power_trace(df, output_file)
     df.drop(columns=["group"], inplace=True)  # Clean up the DataFrame
-    df.to_csv("aggregated_power_data.csv", index=False)
+    df.to_csv("aggregated_power_data_1000stepsync.csv", index=False)
 
 
 if __name__ == "__main__":
