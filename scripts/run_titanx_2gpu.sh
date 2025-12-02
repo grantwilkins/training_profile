6875f922cc03337733b425c0dd63426352d37ffb
Grant <grant.wilkins371@gmail.com>
2025-12-02 20:15:23 +0000

    2gpu trace

--- scripts/run_titanx_2gpu.sh
+++ scripts/run_titanx_2gpu.sh
@@ -5,7 +5,7 @@ LOG=../titanx-traces/power-trace_2gpu_${DATE_TIME}.csv
 
 nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 100 >> "$LOG" &
 SMI_PID=$!
-python train_gpt_simple.py \
+python ../training/train_gpt_simple.py \
   --model_size 125M \
   --batch_size 1 \
   --sequence_length 256 \
