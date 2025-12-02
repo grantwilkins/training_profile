472a007feb235409bb498c609c9382522e0d4fa1
Grant <grant.wilkins371@gmail.com>
2025-12-02 12:12:10 -0800

    move the run2

--- scripts/run_titanx_2gpu.sh
+++ scripts/run_titanx_2gpu.sh
@@ -1,11 +1,19 @@
 conda init
 conda activate titanx
-torchrun --nproc_per_node=2 train_gpt_simple.py \
+DATE_TIME=$(date '+%Y-%m-%d-%H-%M-%S')
+LOG=../titanx-traces/power-trace_2gpu_${DATE_TIME}.csv
+
+nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 100 >> "$LOG" &
+SMI_PID=$!
+python train_gpt_simple.py \
   --model_size 125M \
   --batch_size 1 \
-  --sequence_length 512 \
+  --sequence_length 256 \
   --gradient_accumulation_steps 1 \
-  --precision fp16 \
-  --max_steps 100 \
+  --precision fp32 \
+  --max_steps 200 \
+  --save_steps 100 \
   --log_steps 10 \
   --dataset dummy
+
+kill -9 "$SMI_PID"
