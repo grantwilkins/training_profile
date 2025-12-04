3d71abcbd066beef845c8c6da4e533a57eaf4773
Grant <grant.wilkins371@gmail.com>
2025-12-04 21:29:49 +0000

    finer script

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -1,8 +1,8 @@
 python3 ../profiling/profile.py \
     --gpu 0 \
     --mode sweep \
-    --duty_min 0.1 \
-    --duty_max 1.0 \
-    --duty_step 0.1 \
+    --duty_min 0.75 \
+    --duty_max 0.95 \
+    --duty_step 0.01 \
     --dwell_seconds 5.0 \
-    --csv duty_sweep_gpu0.csv
+    --csv duty_sweep_gpu0_fine.csv
