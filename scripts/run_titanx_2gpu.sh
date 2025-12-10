5f999fe2a73d24423ec561eaf49bc9a50ee88995
Grant <grant.wilkins371@gmail.com>
2025-12-10 17:50:31 +0000

    training titan x last traces

--- scripts/run_titanx_2gpu.sh
+++ scripts/run_titanx_2gpu.sh
@@ -5,12 +5,12 @@ LOG=../titanx-traces/power-trace_2gpu_${DATE_TIME}.csv
 nvidia-smi --query-gpu=timestamp,power.draw,utilization.gpu,memory.used --format=csv -lms 100 >> "$LOG" &
 SMI_PID=$!
 torchrun --nproc_per_node=2 ../training/train_gpt_simple.py \
-  --model_size 125M \
+  --model_size 350M \
   --batch_size 1 \
   --sequence_length 256 \
   --gradient_accumulation_steps 10 \
   --precision fp32 \
-  --max_steps 250 \
+  --max_steps 500 \
   --save_steps 50 \
   --log_steps 10 \
   --dataset wikitext \
